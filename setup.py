f7cc9e988c5e716d08b648af0f8774698134b08b
shak <sovannara.hak@isir.upmc.fr>
2013-01-10 11:30:20 +0100

    Add simple setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='XDE-RobotLoader',
+	  version='0.1',
+	  description='Robot loader util for xde',
+	  author='Soseph',
+	  author_email='hak@isir.upmc.fr',
+	  package_dir={'xde_robot_loader':'src'},
+	  packages=['xde_robot_loader'],
+	 )
