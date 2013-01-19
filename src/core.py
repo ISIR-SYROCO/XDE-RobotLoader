24e5c82321902ec0203f0e8c9c12a53fe11da216
Joseph Salini <salini@isir.upmc.fr>
2013-01-19 16:58:41 +0100

    improve parse_urdf function, add simple_shapes.dae. Remains to take into account mesh transform and scale given in urdf

--- src/core.py
+++ src/core.py
@@ -94,6 +94,8 @@ def Quaternion2RollPitchYaw(Q):
     return rpy
 
 
+def UrdfPose2Displacement(urdfPose):
+    return lgsm.Displacement(lgsm.vector(urdfPose.position) ,RollPitchYaw2Quaternion(*urdfPose.rotation))
 
 
 def createBinding(world, phy_name, graph_node_name, comp_name):
@@ -125,7 +127,7 @@ def createWorldFromUrdfFile(urdfFileName, robotName, H_init=None, is_fixed_base 
     urdfWorld = desc.scene.createWorld(name=robotName+"root")
 
     root_node = urdfWorld.scene.graphical_scene.root_node
-    children_nodes = root_node.children                     #TODO: if structure is not a comb, but a tree
+#    children_nodes = root_node.children                     #TODO: if structure is not a comb, but a tree
 
     urdfRobot, urdfNodes, urdfGraphNodes, urdfCollNodes, urdfMatNodes = parse_urdf(urdfFileName, robotName, minimal_damping)
 
@@ -152,36 +154,41 @@ def createWorldFromUrdfFile(urdfFileName, robotName, H_init=None, is_fixed_base 
     #######################################################
     print "GET GRAPHICAL TREE..."
     binding_phy_graph = {}
-    #for link_name, mesh_filename in urdfGraphNodes.items():
     for link_name in urdfRobot.links.keys():
         robotLinkName = link_name+robotName
+        binding_phy_graph[robotLinkName] = robotLinkName
+
         if robotLinkName in urdfGraphNodes:
-            mesh_filename = urdfGraphNodes[robotLinkName]
-            if mesh_filename is not None:
-                filename, sep, node_in_file = mesh_filename.partition("#")
+            mesh_filename, mesh_position, mesh_scale = urdfGraphNodes[robotLinkName]
+            filename, sep, node_in_file = mesh_filename.partition("#")
 
-                tmp_world = desc.scene.parseColladaFile( str(os.path.dirname(urdfFileName)+"/"+filename ),
-                                                         append_label_library = robotName,
-                                                         append_label_nodes = robotName,
-                                                         append_label_graph_meshes = robotName )
+            tmp_world = desc.scene.parseColladaFile( filename ,
+                                                     append_label_library = robotName,
+                                                     append_label_nodes = robotName,
+                                                     append_label_graph_meshes = robotName )
+            
+            if robotLinkName in urdfMatNodes:
+                mat = urdfWorld.library.materials.add()
+                mat.name = robotLinkName+"_material"
+                desc.material.fillColorMaterial(mat, [ float(x) for x in urdfMatNodes[robotLinkName] ])
+                desc.graphic.applyMaterialSet(tmp_world.scene.graphical_scene.root_node, material_set=[mat.name])
+            else:
+                desc.graphic.applyMaterialSet(tmp_world.scene.graphical_scene.root_node, material_set=["xde/YellowOpaqueAvatars", "xde/GreenOpaqueAvatars", "xde/RedOpaqueAvatars"]) #TODO: delete
 
-                if len(node_in_file) > 0:
-                    if robotLinkName in urdfMatNodes:
-                        mat = urdfWorld.library.materials.add()
-                        mat.name = robotLinkName+"_material"
-                        desc.material.fillColorMaterial(mat, [ float(x) for x in urdfMatNodes[robotLinkName] ])
-                        desc.graphic.applyMaterialSet(tmp_world.scene.graphical_scene.root_node, material_set=[mat.name])
-                    else:
-                        desc.graphic.applyMaterialSet(tmp_world.scene.graphical_scene.root_node, material_set=["xde/YellowOpaqueAvatars", "xde/GreenOpaqueAvatars", "xde/RedOpaqueAvatars"]) #TODO: delete
-                    desc.simple.graphic.addGraphicalTree(urdfWorld, tmp_world, node_in_file+robotName, src_node_name=node_in_file+robotName, ignore_library_conflicts=False)
-                else:
-                    pass # TODO: treat if the whole file is the body (take the root)
-                binding_phy_graph[robotLinkName] = node_in_file+robotName
+            if len(node_in_file) > 0:
+                node_to_copy = node_in_file+robotName
+            else:
+                node_to_copy = "root"+robotName
+            
+            ######################
+            # add body mesh node #
+            ######################
+            desc.simple.graphic.addGraphicalTree(urdfWorld, tmp_world,
+                                                 node_name = robotLinkName, #+"_mesh",                                          # name of of mesh in dest world
+                                                 dest_parent_node_name = robotName+"root", #robotLinkName+"_mesh_transform",    # parent node of mesh in dest world
+                                                 src_node_name=node_to_copy)                                                    # name of node to copy in src world
 
-            else: #if mesh_filename is None, then it is sphere, cube, cyl
-                pass #TODO
         else:
-            binding_phy_graph[robotLinkName] = robotLinkName
             graph_node = desc.graphic.addGraphicalNode(urdfWorld.scene.graphical_scene, name=robotLinkName, parent_node=urdfWorld.scene.graphical_scene.root_node)
             graph_node.position.extend([0,0,0,1,0,0,0])
             graph_node.scale.extend([1,1,1])
@@ -197,24 +204,26 @@ def createWorldFromUrdfFile(urdfFileName, robotName, H_init=None, is_fixed_base 
     for link_name in urdfRobot.links.keys():
         robotLinkName = link_name+robotName
         if robotLinkName in urdfCollNodes:
-            mesh_filename = urdfCollNodes[robotLinkName]
-            if mesh_filename is not None:
-                filename, sep, node_in_file = mesh_filename.partition("#")
+            mesh_filename, mesh_position, mesh_scale = urdfCollNodes[robotLinkName]
+            filename, sep, node_in_file = mesh_filename.partition("#")
 
-                tmp_world = desc.scene.parseColladaFile( str(os.path.dirname(urdfFileName)+"/"+filename ),
-                                                         append_label_library = robotName+'coll',
-                                                         append_label_nodes = robotName,
-                                                         append_label_graph_meshes = robotName )
-                if len(node_in_file) > 0:
-                    composite_name = robotLinkName+".comp"
-                    createComposite(tmp_world, node_in_file+robotName, composite_name, composite_offset)
-                    desc.simple.collision.addCompositeMesh(urdfWorld, tmp_world, composite_name, src_node_name=node_in_file+robotName, offset=composite_offset)
-                else:
-                    pass # TODO: treat if the whole is the body (take the root)
+            tmp_world = desc.scene.parseColladaFile( filename ,
+                                                     append_label_library = robotName+'coll',
+                                                     append_label_nodes = robotName,
+                                                     append_label_graph_meshes = robotName )
 
-                binding_phy_coll[robotLinkName] = composite_name
+            composite_name = robotLinkName+".comp"
+            if len(node_in_file) > 0:
+                node_to_copy = node_in_file+robotName
             else:
-                binding_phy_coll[robotLinkName] = ""
+                node_to_copy = "root"+robotName
+                
+            createComposite(tmp_world, node_to_copy, composite_name, composite_offset)
+            desc.simple.collision.addCompositeMesh(urdfWorld, tmp_world, composite_name, src_node_name=node_to_copy, offset=composite_offset)
+
+
+            binding_phy_coll[robotLinkName] = composite_name
+
         else:
             binding_phy_coll[robotLinkName] = ""
 
@@ -274,67 +283,103 @@ def createWorldFromUrdfFile(urdfFileName, robotName, H_init=None, is_fixed_base 
 
 
 
+simple_shapes_dae = os.path.dirname(__file__) + os.sep + "simple_shapes.dae" # Dont really know where to place it
+
 def parse_urdf(urdfFileName, robotName, minimal_damping):
     """
     """
     import urdf
     robot = urdf.URDF.load_xml_file(urdfFileName)
 
+    phy_nodes      = {}
+    graph_nodes    = {}
+    coll_nodes     = {}
+    material_nodes = {}
 
+
+    ##############################################
+    # Functions defined for parsing robot easily #
+    ##############################################
     def get_joint_from_child_link(child_name):
+        """
+        """
         for j_name, joint in robot.joints.items():
             if joint.child == child_name:
                 return joint
         return None # if not found
 
-    phy_nodes   = {}
-    graph_nodes = {}
-    coll_nodes  = {}
-    material_nodes = {}
+    
+    def get_visual_and_coll_data(linkName, element, dict_to_fill, simple_shape_def):
+        """
+        """
+        if hasattr(element, "geometry"):
+            mesh_origin = UrdfPose2Displacement(element.origin)
+            if isinstance(element.geometry, urdf.Mesh):
+                mesh_scale = (1,1,1)
+                if element.geometry.scale is not None:
+                    mesh_scale_str = element.geometry.scale.split()
+                    if len(mesh_scale_str) == 1:
+                        mesh_scale = [float(mesh_scale_str[0])]*3
+                    elif len(mesh_scale_str) == 3:
+                        mesh_scale = [float(v) for v in mesh_scale_str]
+                    else:
+                        raise ValueError, "Cannot interpret scale '"+element.geometry.scale+"' of link '"+linkName+"' correctly"
+                dict_to_fill[linkName] =  [str(os.path.dirname(urdfFileName) + os.sep + element.geometry.filename), mesh_origin, mesh_scale ]
+            elif isinstance(element.geometry, urdf.Box):
+                dict_to_fill[linkName] = [simple_shapes_dae + "#simple_shape_box", mesh_origin, element.geometry.dims ]
+            elif isinstance(element.geometry, urdf.Sphere):
+                radius = element.geometry.radius
+                dict_to_fill[linkName] = [simple_shapes_dae + "#simple_shape_sphere_"+simple_shape_def, mesh_origin, (radius,radius,radius)]
+            elif isinstance(element.geometry, urdf.Cylinder):
+                length, radius = element.geometry.length, element.geometry.radius
+                dict_to_fill[linkName] = [simple_shapes_dae + "#simple_shape_cylinder_"+simple_shape_def, mesh_origin, (radius,radius,length) ]
 
+
+    ############################################
+    # Parsing all links (bodies) in urdf files #
+    ############################################
+    # The goal is to get the inertial, visual and collision
+    # properties which will be used in the physical nodes of
+    # the world. We do not care about structure here.
+    #
     for l_name, link  in robot.links.items():
-        mass = link.inertial.mass
-        j_from_child  = get_joint_from_child_link(l_name)
-
         robotLinkName =  l_name+robotName
+        mass = link.inertial.mass
 
+        j_from_child  = get_joint_from_child_link(l_name)
         if j_from_child is not None:
-            position = j_from_child.origin.position
-            rotation = j_from_child.origin.rotation
-            H_parent_body = lgsm.Displacement(lgsm.vectord(*position) , RollPitchYaw2Quaternion(*rotation))
-
-            phy_nodes[robotLinkName] = [robotLinkName, mass, H_parent_body, [], []]
+            H_parent_body = UrdfPose2Displacement(j_from_child.origin)
         else:
-            phy_nodes[robotLinkName] = [robotLinkName, mass, lgsm.Displacementd(), [], []]
-        
-        if hasattr(link.visual, "geometry"):
-            if isinstance(link.visual.geometry, urdf.Mesh):
-                graph_nodes[robotLinkName] = link.visual.geometry.filename
-            else:
-                graph_nodes[robotLinkName] = None #TODO: complete with other shapes, box, sphere, etc...
+            H_parent_body = lgsm.Displacementd()
 
-        if hasattr(link.visual, "material"):
-            if link.visual.material.color is not None:
+        phy_nodes[robotLinkName] = [robotLinkName, mass, H_parent_body, [], []] # create a node for the XDE kinematic tree in dict phy_nodes
+
+        get_visual_and_coll_data(robotLinkName, link.visual,    graph_nodes, "high") # fill dict graph_nodes (which bodies has visual)
+        get_visual_and_coll_data(robotLinkName, link.collision, coll_nodes,  "low")  # fill dict coll_nodes  (which bodies has collision)
+
+        if hasattr(link.visual, "material") and link.visual.material.color is not None: # save color, if any
                 material_nodes[robotLinkName] = link.visual.material.color.rgba
 
-        if hasattr(link.collision, "geometry"):
-            if isinstance(link.collision.geometry, urdf.Mesh):
-                coll_nodes[robotLinkName] = link.collision.geometry.filename
-            else:
-                coll_nodes[robotLinkName] = None #TODO: complete with other shapes, box, sphere, etc...
 
-
-    urdf_joint_type_to_xde = {"revolute": "hinge",
-                              } #TODO: to complete
-
+    ####################################
+    # Parsing all joints in urdf files #
+    ####################################
+    # The goal is to get the structure of the robot, the relation
+    # between bodies, type of joints, axis, etc...
+    # At the end, we fill phy_nodes dict to create the kinematic tree
+    # for the world in XDE.
+    #
     for j_name, joint in robot.joints.items():
         p_name     = joint.parent
         c_name     = joint.child
-        V_p_joint  = joint.origin.position  #[0,0,0] #
-        A_c_joint  = [float(v) for v in joint.axis.split()] #TODO:PB with parser
-        qmin       = joint.limits.lower  if hasattr(joint.limits, "lower") else round(-4*np.pi)
-        qmax       = joint.limits.upper  if hasattr(joint.limits, "upper") else  round(4*np.pi)
-        tau_max    = joint.limits.effort if hasattr(joint.limits, "effort") else 10000.
+        assert(p_name in robot.links), p_name+" is not listed in the urdf links..."
+        assert(c_name in robot.links), c_name+" is not listed in the urdf links..."
+        
+        V_p_joint  = joint.origin.position
+        A_c_joint  = [float(v) for v in joint.axis.split()] #TODO:PB with parser, it returns a string and not a tuple
+        qmin       = joint.limits.lower     if hasattr(joint.limits, "lower")     else -12.
+        qmax       = joint.limits.upper     if hasattr(joint.limits, "upper")     else  12.
+        tau_max    = joint.limits.effort    if hasattr(joint.limits, "effort")    else 10000.
         joint_damp = joint.dynamics.damping if hasattr(joint.dynamics, "damping") else minimal_damping
         qinit      = 0 #TODO: no mean to give init value of q
         
@@ -343,13 +388,22 @@ def parse_urdf(urdfFileName, robotName, minimal_damping):
         R_p_c     = RollPitchYaw2Quaternion(*joint.origin.rotation)
         A_p_joint = R_p_c * A_c_joint
         A_p_joint.resize(3)
-        A_p_joint = [round(v, 10) for v in A_p_joint]
-        
-        if joint.joint_type in ["revolute"]: #TODO: to complete
-            phy_nodes[c_name+robotName][3].append(  (urdf_joint_type_to_xde[joint.joint_type], V_p_joint, A_p_joint, joint_damp, qmin, qmax, qinit)   )
+        #A_p_joint = [round(v, 10) for v in A_p_joint] TODO: useless I suppose
+
+
+        #possible urdf joint type = {"revolute","continuous","prismatic","fixed", "planar", "floating (deprecated)"}
+        if joint.joint_type in ["revolute", "prismatic"]:
+            jType = "hinge" if joint.joint_type == "revolute" else "prismatic"
+            phy_nodes[c_name+robotName][3].append(  (jType, V_p_joint, A_p_joint, joint_damp, qmin, qmax, qinit)   )
             phy_nodes[p_name+robotName][4].append(phy_nodes[c_name+robotName])
+        elif joint.joint_type == "fixed":
+            raise ValueError(" more test needed before using this type of joint")
+            phy_nodes[c_name+robotName][3].append( [] )
+            phy_nodes[p_name+robotName][4].append(phy_nodes[c_name+robotName])
+        elif joint.joint_type in ["continuous", "planar", "floating"]:
+            raise ValueError("joint type '"+joint.joint_type+"' is in urdf convention, but it is not managed with this loader...")
         else:
-            raise ValueError
+            raise ValueError("joint type '"+joint.joint_type+"' is NOT in urdf convention. Invalid URDF.")
 
 
     return robot, phy_nodes, graph_nodes, coll_nodes, material_nodes
