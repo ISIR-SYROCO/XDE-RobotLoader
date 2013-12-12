84aae568dba26987313d492c2d768ae217a713c0
shak <sovannara.hak@isir.upmc.fr>
2013-12-12 16:04:07 +0100

    Use IsirPythonTools submodule as setup script

--- setup.py
+++ setup.py
@@ -1,80 +1,14 @@
-from distutils.core import setup, Command
-
-import os, sys, string, shutil, errno
-from site import USER_BASE
+from IsirPythonTools import *
 
 package_name = 'xde_robot_loader'
-
-def force_symlink(file1, file2):
-	try:
-		os.symlink(file1, file2)
-	except OSError, e:
-		if e.errno == errno.EEXIST:
-			shutil.rmtree(file2)
-			os.symlink(file1, file2)
-
-class develop(Command):
-	description = "Create symbolic link instead of installing files"
-	user_options = [
-			('prefix=', None, "installation prefix"),
-			('uninstall', None, "uninstall development files")
-			]
-
-	def initialize_options(self):
-		self.prefix = None
-		self.uninstall = 0
-
-	def finalize_options(self):
-		self.py_version = (string.split(sys.version))[0]
-		if self.prefix is None:
-		    self.prefix = USER_BASE
-		self.prefix = os.path.expanduser(self.prefix)
-
-	def run(self):
-		out_dir = os.path.join(self.prefix, "lib", "python"+self.py_version[0:3], "site-packages")
-		if not os.path.isdir(out_dir):
-			os.makedirs(out_dir)
-
-		out_dir = os.path.join(out_dir, package_name)
-		src_dir = os.path.join(os.getcwd(), "src" )
-		if self.uninstall == 1:
-			if os.path.islink(out_dir):
-				print "Removing symlink "+out_dir
-				os.remove(out_dir)
-			else:
-				print "Not in dev mode, nothing to do"
-		else:
-			if os.path.islink(out_dir):
-				print "Already in dev mode"
-			else:
-				print "Creating symlink "+src_dir+" -> "+out_dir
-				force_symlink(src_dir, out_dir)
-
-cmdclass={'develop': develop}
-
-try:
-	# add a command for building the html doc
-	from sphinx.setup_command import BuildDoc
-	cmdclass['build_doc'] = BuildDoc
-except ImportError:
-	pass
-
-
-if sys.argv[0] == "setup.py":
-    script_name=sys.argv[0]
-    script_args=sys.argv[1:]
-else:
-    script_name=sys.argv[1]
-    script_args=sys.argv[2:]
-
 
 setup(name='XDE-RobotLoader',
 	  version='0.1',
 	  description='Robot loader util for xde',
 	  author='Soseph',
 	  author_email='hak@isir.upmc.fr',
-	  package_dir={'xde_robot_loader':'src'},
-	  package_data={'xde_robot_loader': ['simple_shapes.dae']},
+	  package_dir={package_name:'src'},
+	  package_data={package_name: ['simple_shapes.dae']},
 	  packages=[package_name],
 	  cmdclass=cmdclass,
 
